32a33508b24c3d7d29cf7dd4ef3bb97ce8605e83
Christopher Selig <Chris.Selig0@gmail.com>
2022-03-26 00:14:41 -0400

    Added favicon, css changes, and improved layout.

--- index.php
+++ index.php
@@ -2,8 +2,24 @@
 <html lang="en">
     <head>
         <title>Rss Reader</title>
+        <meta name="viewport" content="width=device-width">
+        <meta name="description" content="A Simple RSS Reader">
+        <meta name="keywords" content="RSS, CSS, HTML, PHP">
+        <meta name="author" content="Christopher Selig">
+        <link rel="stylesheet" href="/css/index.css">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
     </head>
-    <body>
-        
+    <body style="margin: 0;">
+        <div id="header">
+            <h1 id="title" style="font-size: 200%; margin: 0">Simple RSS Reader</h1>
+        </div>
+        <div id="main-content">
+        <form>
+            <input id="url" type="text" placeholder="RSS Feed URL">
+        </form>
+            <section id="feed">
+            Some placeholder bs
+            </section>
+        </div>
     </body>
 </html>
